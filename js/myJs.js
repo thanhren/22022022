21106add2cbec8aa24da0e31d287567ab2978a8f
thanhren <40245250+thanhren@users.noreply.github.com>
2021-11-25 11:15:07 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,18 +1,19 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
+   text1: "Happy Birthday To You",
+  text2: "Chúc mừng giáng sinh vui vẻ :))",
+  text3: "Chúc Thùy Hằng có một ngày sinh nhật vui vẻ",
+  text4: "Và nhiều ngày sau cũng như thế!",
+  text5: "Xin lỗi, không cần",
+  text6: "Cảm ơn rất nhiều ^^!",
+  text7: "Click vào phía dưới xem thử có gì!",
+  text8: "Nhấn vào đây nè.",
   text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text10: "Happy birthday to you",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Chúc Hằng một tuổi mới đầy những điều may mắn nhé! ",
+  text12: "Nghe một bài nhạc tận hưởng ngày sinh nhật.",
 };
+
 
 $(document).ready(function () {
   // process bar
